d15f8ff9289a9f2722cf5b47760efa47d63f7c11
Guribolte <101675322+Guribolte@users.noreply.github.com>
2023-01-20 07:57:58 +0100

    Update play.py

--- YukkiMusic/utils/inline/play.py
+++ YukkiMusic/utils/inline/play.py
@@ -7,32 +7,40 @@
 #
 # All rights reserved.
 
-import random
+import math
 
 from pyrogram.types import InlineKeyboardButton
-
-selections = [
-    "▁▄▂▇▄▅▄▅▃",
-    "▁▃▇▂▅▇▄▅▃",
-    "▃▁▇▂▅▃▄▃▅",
-    "▃▄▂▄▇▅▃▅▁",
-    "▁▃▄▂▇▃▄▅▃",
-    "▃▁▄▂▅▃▇▃▅",
-    "▁▇▄▂▅▄▅▃▄",
-    "▁▃▅▇▂▅▄▃▇",
-    "▃▅▂▅▇▁▄▃▁",
-    "▇▅▂▅▃▄▃▁▃",
-    "▃▇▂▅▁▅▄▃▁",
-    "▅▄▇▂▅▂▄▇▁",
-    "▃▅▂▅▃▇▄▅▃",
-]
+import config
+from YukkiMusic.utils.formatters import time_to_seconds
 
 
 ## After Edits with Timer Bar
 
-
 def stream_markup_timer(_, videoid, chat_id, played, dur):
-    bar = random.choice(selections)
+    played_sec = time_to_seconds(played)
+    duration_sec = time_to_seconds(dur)
+    percentage = (played_sec / duration_sec) * 100
+    anon = math.floor(percentage)
+    if 0 < Yukki <= 10:
+        bar = "⬤─────────"
+    elif 10 < Yukki < 20:
+        bar = "━⬤────────"
+    elif 20 <= Yukki < 30:
+        bar = "━━⬤───────"
+    elif 30 <= Yukki < 40:
+        bar = "━━━⬤──────"
+    elif 40 <= Yukki < 50:
+        bar = "━━━━⬤─────"
+    elif 50 <= Yukki < 60:
+        bar = "━━━━━⬤────"
+    elif 60 <= Yukki < 70:
+        bar = "━━━━━━⬤───"
+    elif 70 <= Yukki < 80:
+        bar = "━━━━━━━⬤──"
+    elif 80 <= Yukki < 95:
+        bar = "━━━━━━━━⬤─"
+    else:
+        bar = "━━━━━━━━━⬤"
     buttons = [
         [
             InlineKeyboardButton(
@@ -60,7 +68,30 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
 
 
 def telegram_markup_timer(_, chat_id, played, dur):
-    bar = random.choice(selections)
+    played_sec = time_to_seconds(played)
+    duration_sec = time_to_seconds(dur)
+    percentage = (played_sec / duration_sec) * 100
+    anon = math.floor(percentage)
+    if 0 < Yukki <= 10:
+        bar = "⬤─────────"
+    elif 10 < Yukki < 20:
+        bar = "━⬤────────"
+    elif 20 <= Yukki < 30:
+        bar = "━━⬤───────"
+    elif 30 <= Yukki < 40:
+        bar = "━━━⬤──────"
+    elif 40 <= Yukki < 50:
+        bar = "━━━━⬤─────"
+    elif 50 <= Yukki < 60:
+        bar = "━━━━━⬤────"
+    elif 60 <= Yukki < 70:
+        bar = "━━━━━━⬤───"
+    elif 70 <= Yukki < 80:
+        bar = "━━━━━━━⬤──"
+    elif 80 <= Yukki < 95:
+        bar = "━━━━━━━━⬤─"
+    else:
+        bar = "━━━━━━━━━⬤"
     buttons = [
         [
             InlineKeyboardButton(
